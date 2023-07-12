76f36cc7526398c620e1a7f9c67903d648a02a2a
mmlocc <111124140+mmlocc@users.noreply.github.com>
2023-07-12 19:43:52 +0900

    동그란 프로필 위에 나오는 이름 삭제
    이름을 어떻게 나오게 할지 더 고민 해봐야할듯해서 삭제합니다!

--- lib/main.dart
+++ lib/main.dart
@@ -147,7 +147,7 @@ class MyWidget extends StatelessWidget {
                     child: Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        "이 사람들이 더 궁금하다면?\n송정음        정동교       김남인       허수빈",
+                        "이 사람들이 더 궁금하다면?",
                         style: TextStyle(
                           fontSize: 22,
                           fontWeight: FontWeight.bold,
